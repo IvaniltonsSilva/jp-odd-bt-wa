8026beb5a1de4a4af81bfb070baa2ee32ef24216
JCPrime01 <vanzokiller@gmail.com>
2026-07-06 10:50:49 -0300

    chore: shareCode MYC1NNZ9CFF -> UAUDZJTQPZ9 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=MYC1NNZ9CFF'
+    const shareCode = '&shareCode=UAUDZJTQPZ9'
     
     const afp = '&afp=al'
 
