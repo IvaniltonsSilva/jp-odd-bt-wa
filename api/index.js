ef8e649cc01133c4f2b9f0666894d67d3d1b1fab
JCPrime01 <vanzokiller@gmail.com>
2026-07-13 18:56:33 -0300

    chore: shareCode JFG2JBSYRVO -> R2YIPWSK2QQ (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=JFG2JBSYRVO'
+    const shareCode = '&shareCode=R2YIPWSK2QQ'
     
     const afp = '&afp=al'
 
