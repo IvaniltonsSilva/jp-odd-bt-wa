6521c3ad0f4edca509680ba167e7dc1c35f04269
JCPrime01 <vanzokiller@gmail.com>
2026-08-06 15:17:05 -0300

    chore: shareCode E29FTIEU627 -> NGKC1G4VZ1L (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=E29FTIEU627'
+    const shareCode = '&shareCode=NGKC1G4VZ1L'
     
     const afp = '&afp=al'
 
