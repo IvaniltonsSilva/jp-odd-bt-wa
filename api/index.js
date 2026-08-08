b9264b4794de78edd01e44e62b791492b09d9ab5
JCPrime01 <vanzokiller@gmail.com>
2026-08-08 13:05:13 -0300

    chore: shareCode NGKC1G4VZ1L -> ZHWTVZQZ40R (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=NGKC1G4VZ1L'
+    const shareCode = '&shareCode=ZHWTVZQZ40R'
     
     const afp = '&afp=al'
 
